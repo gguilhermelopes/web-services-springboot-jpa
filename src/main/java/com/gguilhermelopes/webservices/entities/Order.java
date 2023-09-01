c97020ee782170a209491da8f683ef1da1db0053
gguilhermelopes <guilhermelopes275@gmail.com>
2023-09-01 10:13:15 -0300

    feat: set ENUM to OrderStatus

--- src/main/java/com/gguilhermelopes/webservices/entities/Order.java
+++ src/main/java/com/gguilhermelopes/webservices/entities/Order.java
@@ -2,6 +2,7 @@ package com.gguilhermelopes.webservices.entities;
 
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.gguilhermelopes.webservices.entities.enums.OrderStatus;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
@@ -14,17 +15,18 @@ public class Order implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-   
     private Instant moment;
+    private Integer orderStatus;
     @ManyToOne
     @JoinColumn(name = "client_id")
     private User client;
 
     public Order(){}
 
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
         this.id = id;
         this.moment = moment;
+        setOrderStatus(orderStatus);
         this.client = client;
     }
 
@@ -42,6 +44,16 @@ public class Order implements Serializable {
 
     public void setMoment(Instant moment) {
         this.moment = moment;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if(orderStatus != null) {
+            this.orderStatus = orderStatus.getCode();
+        }
     }
 
     public User getClient() {
