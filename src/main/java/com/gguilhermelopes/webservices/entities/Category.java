215d8bd29dde519751662fbaf5c322fc7cccb5c8
gguilhermelopes <guilhermelopes275@gmail.com>
2023-09-04 12:35:21 -0300

    feat: many-to-many with join table of categories and products

--- src/main/java/com/gguilhermelopes/webservices/entities/Category.java
+++ src/main/java/com/gguilhermelopes/webservices/entities/Category.java
@@ -1,5 +1,6 @@
 package com.gguilhermelopes.webservices.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
@@ -14,7 +15,8 @@ public class Category implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
-    @Transient
+    @ManyToMany(mappedBy = "categories")
+    @JsonIgnore
     private Set<Product> products = new HashSet<>();
 
     public Category(){}
