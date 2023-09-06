a5a821c4fefc05a5dfff9ad29b39cace039b99b9
gguilhermelopes <guilhermelopes275@gmail.com>
2023-09-06 15:50:36 -0300

    refactor: delete unused import

--- src/main/java/com/gguilhermelopes/webservices/services/UserService.java
+++ src/main/java/com/gguilhermelopes/webservices/services/UserService.java
@@ -8,7 +8,6 @@ import com.gguilhermelopes.webservices.services.exceptions.ResourceNotFoundExcep
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
